f7168f884ec483135c7923f751a96dbd6a3fba5f
algorithm-sys <marlvex369@gmail.com>
2024-08-03 10:14:09 +0300

    Fsix

--- manage.py
+++ manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 import os
 import sys
+import dotenv
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phonecentre.settings.dev")
+    dotenv.load_dotenv()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phonecentre.settings.production")
 
     from django.core.management import execute_from_command_line
 
