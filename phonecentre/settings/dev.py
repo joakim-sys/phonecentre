59e613537d500161fca2d86aa9d84c483038d8cf
algorithm-sys <marlvex369@gmail.com>
2024-08-03 10:09:13 +0300

    Fsix

--- phonecentre/settings/dev.py
+++ phonecentre/settings/dev.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-yol2ip38a3t&9_bwx=uio4b9*7mxfcvcurj741(*$@v*zs3j3f"
--- phonecentre/wsgi.py
+++ phonecentre/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
+import dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phonecentre.settings.dev")
+dotenv.load_dotenv(os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env"))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "phonecentre.settings.production")
 
 application = get_wsgi_application()
