0294871bf18e7c8fd5bb8eb30658b09608473070
algorithm-sys <marlvex369@gmail.com>
2024-08-03 12:37:36 +0300

    Fsix

--- phonecentre/urls.py
+++ phonecentre/urls.py
@@ -6,10 +6,6 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
-from django.conf.urls.static import static
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
-
 from search import views as search_views
 
 urlpatterns = [
@@ -21,6 +17,8 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
